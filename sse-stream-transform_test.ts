8b6d36bf2c6255c43c37261db4ab06d5636de56d
Ziv Perry <ziv.perry@zoominfo.com>
2025-10-22 12:43:02 +0300

    add test description

--- sse-stream-transform_test.ts
+++ sse-stream-transform_test.ts
@@ -2,7 +2,11 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import {SseStreamTransform} from "./sse-stream-transform.js";
 
-const input = `
+
+// In order to use the "done" in the proper place, we cannot use async functions directly in the test.
+// So we wrap the test logic in an async IIFE and call done() when finished.
+test("Transform raw SSE stream into a stream of SSE objects", (_, done) => {
+    const input = `
 data: message 1 part 1
 data: message 1 part 2
 
@@ -13,34 +17,35 @@ data: message 3
 
 `;
 
-test("Sample test for SSE transform", (_, done) => {
-  const encoder = new TextEncoder();
-  const transformer = new SseStreamTransform();
-  const reader = transformer.readable.getReader();
-  const writer = transformer.writable.getWriter();
+    const encoder = new TextEncoder();
+    const transformer = new SseStreamTransform();
+    const reader = transformer.readable.getReader();
+    const writer = transformer.writable.getWriter();
 
-  (async () => {
-    // convert the console log to assertions
-    const msg1 = await reader.read();
-    assert.equal(msg1.value?.data, "message 1 part 1message 1 part 2");
+    (async () => {
+        // convert the console log to assertions
+        const msg1 = await reader.read();
+        assert.equal(msg1.value?.data, "message 1 part 1message 1 part 2");
 
-    const msg2 = await reader.read();
-    assert.equal(msg2.value?.data, "message 2");
+        const msg2 = await reader.read();
+        assert.equal(msg2.value?.data, "message 2");
 
-    const msg3 = await reader.read();
-    assert.equal(msg3.value?.data, "message 3");
-    assert.equal(msg3.value?.event, "custom");
+        const msg3 = await reader.read();
+        assert.equal(msg3.value?.data, "message 3");
+        assert.equal(msg3.value?.event, "custom");
 
-    const msg4 = await reader.read();
-    assert.equal(msg4.done, true);
-    done();
-  })().catch(console.error);
+        const msg4 = await reader.read();
+        assert.equal(msg4.done, true);
 
-  (async () => {
-    // write input data chunk by chunk (in this case, letter by letter)
-    for (const c of input) {
-      await writer.write(encoder.encode(c));
-    }
-    await writer.close();
-  })().catch(console.error);
+        // now it safe to call done()
+        done();
+    })();
+
+    (async () => {
+        // write input data chunk by chunk (in this case, letter by letter)
+        for (const c of input) {
+            await writer.write(encoder.encode(c));
+        }
+        await writer.close();
+    })();
 });
